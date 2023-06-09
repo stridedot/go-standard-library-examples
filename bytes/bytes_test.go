49f5c088444bb488da20bf3af872d736bd74689f
stidedot <1195775472@qq.com>
2023-06-09 09:48:52 +0800

    bytes示例

--- bytes/bytes_test.go
+++ bytes/bytes_test.go
@@ -2,6 +2,7 @@ package bytes_test
 
 import (
 	"bytes"
+	"io"
 	"testing"
 	"unicode"
 )
@@ -191,6 +192,522 @@ func TestIndexAny(t *testing.T) {
 	t.Logf("index %d\n", bytes.IndexAny(s, chars))
 }
 
+// TestIndexByte 返回字节在 s 切片中第一次出现的位置索引
 func TestIndexByte(t *testing.T) {
+	s := []byte("Good morning")
+	t.Logf("First instance of c in b is %d\n", bytes.IndexByte(s, 'o'))
+}
 
+// TestIndexFunc 返回 s 中第一个满足 f(c) 的 Unicode 代码点的字节索引，如果没有就返回 -1
+func TestIndexFunc(t *testing.T) {
+	s := []byte("Go programming 编程")
+	f := func(r rune) bool {
+		return unicode.Is(unicode.Han, r)
+	}
+	t.Logf("index = %d\n", bytes.IndexFunc(s, f))
+}
+
+// TestIndexRune 返回 rune 字符在 s 中第一次出现的索引
+func TestIndexRune(t *testing.T) {
+	s := []byte("从前有座山，山上有座庙，庙里有个老和尚")
+	t.Logf("index = %d\n", bytes.IndexRune(s, '有'))
+}
+
+// TestJoin 使用 sep 拼接 s 切片
+func TestJoin(t *testing.T) {
+	s := [][]byte{[]byte("bar"), []byte("foo"), []byte("baz")}
+	t.Logf("join: %s\n", bytes.Join(s, []byte(", ")))
+}
+
+// TestLastIndex 返回 s 中 sep 最后一次出现的索引
+func TestLastIndex(t *testing.T) {
+	s := []byte("go gopher")
+	t.Logf("LastIndex: %d\n", bytes.LastIndex(s, []byte("go")))
+}
+
+// TestLastIndexAny 返回 chars 中任一个字符在 s 中最后一次出现的字节索引
+func TestLastIndexAny(t *testing.T) {
+	s := []byte("go 地鼠")
+	t.Logf("LastIndexAny: %d\n", bytes.LastIndexAny(s, "地球"))
+}
+
+// TestLastIndexByte 返回字节 c 在 s 中最后一次出现的字节索引
+func TestLastIndexByte(t *testing.T) {
+	s := []byte("go gopher")
+	t.Logf("LastIndexByte: %d\n", bytes.LastIndexByte(s, 'o'))
+}
+
+// TestLastIndexFunc 返回满足 f(c) 的最后一个字符在 s 中的索引
+func TestLastIndexFunc(t *testing.T) {
+	s := []byte("go gopher！")
+	t.Logf("LastIndexFunc: %d\n", bytes.LastIndexFunc(s, unicode.IsLetter))
+	t.Logf("LastIndexFunc: %d\n", bytes.LastIndexFunc(s, unicode.IsPunct))
+	t.Logf("LastIndexFunc: %d\n", bytes.LastIndexFunc(s, unicode.IsNumber))
+}
+
+// TestMap 对所有字符根据函数进行修改
+func TestMap(t *testing.T) {
+	s := []byte("There are three children in the park")
+	f := func(r rune) rune {
+		if r == 'e' {
+			return 'd'
+		}
+		return r
+	}
+	b := bytes.Map(f, s)
+	t.Logf("bytes.Map: %s\n", b)
+}
+
+// TestRepeat 返回有 b 的 count 个副本组成的新切片
+func TestRepeat(t *testing.T) {
+	t.Logf("repeat: %s\n", bytes.Repeat([]byte("# "), 5))
+}
+
+// TestReplace 将 s 中的前 n 个 k 替换为 ky
+func TestReplace(t *testing.T) {
+	s := []byte("oink oink oink")
+	t.Logf("replace: %s\n", bytes.Replace(s, []byte("k"), []byte("ky"), -1))
+}
+
+// TestReplaceAll 将 s 中的所有 k 替换为 ky
+func TestReplaceAll(t *testing.T) {
+	s := []byte("oink oink oink")
+	t.Logf("replaceAll: %s\n", bytes.ReplaceAll(s, []byte("k"), []byte("ky")))
+}
+
+// TestRunes 将 byte 字符转为 rune 字符
+func TestRunes(t *testing.T) {
+	s := []byte("go gopher")
+	t.Logf("bytes.Runes: %v\n", string(bytes.Runes(s)))
+}
+
+// TestSplit 将 s 按照 sep 分隔，返回子切片
+func TestSplit(t *testing.T) {
+	s := []byte("a,b,c")
+	t.Logf("bytes.Split: %q\n", bytes.Split(s, []byte(",")))
+	t.Logf("bytes.Split: %q\n", bytes.Split([]byte(" abc "), []byte("")))
+	t.Logf("bytes.Split: %q\n", bytes.Split([]byte(""), []byte("")))
+}
+
+// TestSplitAfter 将 s 按照 sep 分割，返回带有 sep 的子切片
+func TestSplitAfter(t *testing.T) {
+	t.Logf("bytes.SplitAfter: %q\n", bytes.SplitAfter([]byte("a,b,c"), []byte(",")))
+}
+
+// TestSplitAfterN 将 s 按照 sep 分割，返回 n 个带有 sep 的子切片
+func TestSplitAfterN(t *testing.T) {
+	// output: ["a," "b,c"]
+	t.Logf("bytes.SplitAfterN: %q\n", bytes.SplitAfterN([]byte("a,b,c"), []byte(","), 2))
+}
+
+// TestSplitN 将 s 按照 sep 分隔，返回 n 个子切片
+func TestSplitN(t *testing.T) {
+	t.Logf("bytes.SplitN: %q\n", bytes.SplitN([]byte("a,b,c"), []byte(","), 2))
+}
+
+// TestToLower 将 s 转为小写
+func TestToLower(t *testing.T) {
+	t.Logf("bytes.ToLower: %s\n", bytes.ToLower([]byte("ABC")))
+}
+
+// TesToLowerSpecial 将 s 转为小写，优先考虑指定小写规则
+func TestToLowerSpecial(t *testing.T) {
+	t.Logf("bytes.ToLowerSpecial: %s\n", bytes.ToLowerSpecial(unicode.AzeriCase, []byte("ahoj vývojári golang")))
+}
+
+// TestToTitle 转为大写，注意 ToTitle 和 ToUpper 的区别
+func TestToTitle(t *testing.T) {
+	t.Logf("bytes.ToTitle: %s\n", bytes.ToTitle([]byte("ǳ")))
+	t.Logf("bytes.ToUpper: %s\n", bytes.ToUpper([]byte("ǳ")))
+}
+
+// TestToTitleSpecial 转为大写，优先考虑大写规则
+func TestToTitleSpecial(t *testing.T) {
+	s := []byte("ahoj vývojári golang")
+	t.Logf("bytes.ToTitleSpecial:%s\n", bytes.ToTitleSpecial(unicode.AzeriCase, s))
+}
+
+func TestToUpper(t *testing.T) {
+	t.Logf("bytes.ToUpper:%s\n", bytes.ToUpper([]byte("Gopher")))
+}
+
+func TestToUpperSpecial(t *testing.T) {
+	t.Logf("bytes.ToUpperSpecial:%s\n", bytes.ToUpperSpecial(unicode.AzeriCase, []byte("ahoj vývojári golang")))
+}
+
+// TesToValidUTF8 将无效 utf-8 的字节替换为指定字节
+func TestToValidUTF8(t *testing.T) {
+	s := bytes.ToValidUTF8([]byte("typical"), []byte("\uFFFD"))
+	t.Logf("s = %s\n", s)
+
+	s = bytes.ToValidUTF8([]byte("foo\xffbar"), []byte("\uFFFD"))
+	t.Logf("s = %s\n", s)
+
+	s = bytes.ToValidUTF8([]byte("日本語\xff日本語"), []byte("\uFFFD"))
+	t.Logf("s = %s\n", s)
+}
+
+// TestTrim 将 s 两端指定的字符移除
+func TestTrim(t *testing.T) {
+	t.Logf("%s\n", bytes.Trim([]byte("!!! Achtung! Achtung! !!!"), "! "))
+}
+
+// TestTrimFun 将 s 按指定函数移除两端字符
+func TestTrimFunc(t *testing.T) {
+	t.Logf("IsLetter: %s\n", bytes.TrimFunc([]byte("go-gopher!"), unicode.IsLetter))
+	t.Logf("IsLetter: %s\n", bytes.TrimFunc([]byte("\"go-gopher!\""), unicode.IsLetter))
+	t.Logf("IsPunct: %s\n", bytes.TrimFunc([]byte("go-gopher!"), unicode.IsPunct))
+	t.Logf("IsNumber: %s\n", bytes.TrimFunc([]byte("1234go-gopher!567"), unicode.IsNumber))
+}
+
+// TestTrimLeft 移除 s 左边字符
+func TestTrimLeft(t *testing.T) {
+	t.Logf("%s\n", bytes.TrimLeft([]byte("123go-gopher!"), "012134"))
+}
+
+// TestTrimLeftFunc 将 s 按指定函数移除左端字符
+func TestTrimLeftFunc(t *testing.T) {
+	t.Logf("IsNumber: %s\n", bytes.TrimLeftFunc([]byte("1234go-gopher!567"), unicode.IsNumber))
+}
+
+// TestTrimPrefix 返回 s 中没有指定前缀的部分
+func TestTrimPrefix(t *testing.T) {
+	t.Logf("trimPrefix:%s\n", bytes.TrimPrefix([]byte("Goodbye, teacher"), []byte("Good")))
+}
+
+// TestTrimSpace 移除 s 中两端的空白
+func TestTrimSpace(t *testing.T) {
+	t.Logf("bytes.TrimSpace: %s\n", bytes.TrimSpace([]byte(" \t\n a lone gopher \n\t\r\n")))
+}
+
+// TestTrimSuffix 返回 s 中没有指定后缀的部分
+func TestTrimSuffix(t *testing.T) {
+	t.Logf("bytes.TrimSuffix:%s\n", bytes.TrimSuffix([]byte("Goodbye, teacher"), []byte("er")))
+}
+
+// TestNewBuffer 初始化一个 buffer
+func TestNewBuffer(t *testing.T) {
+	buf := bytes.NewBuffer([]byte("123456"))
+	var data [10]byte
+	n, err := buf.Read(data[:])
+	if n != 6 {
+		t.Fatalf("Read error: %v\n", err)
+	}
+}
+
+// TestNewBufferString 初始化一个以字符串为参数的新 buffer
+func TestNewBufferString(t *testing.T) {
+	buf := bytes.NewBufferString("abbbaaaba")
+	var data [10]byte
+	n, err := buf.Read(data[:])
+	if n != 9 {
+		t.Fatalf("Read error: %v\n", err)
+	}
+}
+
+// TestBufferBytes 返回一个切片
+func TestBufferBytes(t *testing.T) {
+	buf := bytes.Buffer{}
+	buf.Write([]byte{'h', 'e', 'l', 'l', 'o'})
+	t.Logf("buf.Bytes: %s\n", buf.Bytes())
+}
+
+// TestBufferCap 返回切片的容量
+func TestBufferCap(t *testing.T) {
+	buf1 := bytes.NewBuffer(make([]byte, 10))
+	buf2 := bytes.NewBuffer(make([]byte, 0, 10))
+	t.Logf("buf1 cap = %d\n", buf1.Cap())
+	t.Logf("buf2 cap = %d\n", buf2.Cap())
+}
+
+// TestBufferGroup 增加缓冲区的容量
+func TestBufferGrow(t *testing.T) {
+	var buf bytes.Buffer
+	t.Logf("buf = %q\n", buf.Bytes())
+
+	buf.Grow(64)
+	buf.Write([]byte("more or fewer"))
+	t.Logf("buf = %q\n", buf.Bytes())
+}
+
+// TestBufferLen 返回缓冲区的字节数
+func TestBufferLen(t *testing.T) {
+	buf := bytes.NewBuffer([]byte("abc"))
+	if buf.Len() != len(buf.Bytes()) {
+		t.Fatalf("Want %d, got %d", buf.Len(), len(buf.Bytes()))
+	}
+}
+
+// TestBufferNext 返回缓冲区中 n 个字节数的切片
+func TestBufferNext(t *testing.T) {
+	buf := bytes.NewBuffer([]byte("123456"))
+	t.Logf("next = %s\n", buf.Next(2))
+	t.Logf("next = %s\n", buf.Next(2))
+	t.Logf("bytes.Bytes = %s, len = %d\n", buf.Bytes(), buf.Len())
+}
+
+// TestBufferRead 返回读取的字节
+func TestBufferRead(t *testing.T) {
+	var b bytes.Buffer
+	b.Grow(15)
+	b.Write([]byte("123456"))
+
+	buf := make([]byte, 7)
+	n, err := b.Read(buf)
+	if err != nil {
+		t.Fatalf("Got err: %v", err)
+	}
+
+	buf1 := make([]byte, 7)
+	n, err = b.Read(buf1)
+	if err != io.EOF {
+		t.Fatalf("Want empty, got %s\n", buf1[:n])
+	}
+}
+
+// TestBufferReadByte 测试返回字节
+func TestBufferReadByte(t *testing.T) {
+	s := make([]byte, 10)
+	buf := bytes.NewBuffer([]byte("123456"))
+
+	for {
+		b, err := buf.ReadByte()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			t.Fatalf("buf.ReadByte err: %v\n", err)
+		}
+		s = append(s, b)
+	}
+
+	t.Logf("s = %s\n", s)
+}
+
+// TestBufferReadBytes 测试返回字节数组
+func TestBufferReadBytes(t *testing.T) {
+	buf := bytes.NewBuffer([]byte("bar,foo,baz"))
+	line, err := buf.ReadBytes(',')
+	if err != nil {
+		t.Fatalf("buf.ReadBytes err: %v\n", err)
+	}
+	// output "bar,"
+	t.Logf("buf.ReadBytes = %s\n", line)
+}
+
+// TestBufferReadFrom 从 reader 中读取数据
+func TestBufferReadFrom(t *testing.T) {
+	b := bytes.Buffer{}
+	buf := bytes.NewBuffer([]byte("123456"))
+	_, err := b.ReadFrom(buf)
+	if err != nil {
+		t.Fatalf("b.ReadFrom err: %v\n", err)
+	}
+	t.Logf("b = %s\n", b.String())
+}
+
+// TestBufferReadRune 返回一个 utf-8 编码的字符
+func TestBufferReadRune(t *testing.T) {
+	b := bytes.NewBuffer([]byte("你好，世界"))
+	c, size, err := b.ReadRune()
+	if err != nil {
+		t.Fatalf("b.ReadRune err: %v\n", err)
+	}
+	t.Logf("c = %s, size = %d\n", string(c), size)
+}
+
+// TestBufferReadString 返回包含 delim 第一次出现的字符数据
+func TestBufferReadString(t *testing.T) {
+	s := "gopher$phper$rust"
+	b := bytes.NewBufferString(s)
+
+	line, err := b.ReadString('$')
+	t.Logf("err = %v\n", err)
+	t.Logf("line = %v\n", line)
+}
+
+// TestBufferReset 将缓冲区重置
+func TestBufferReset(t *testing.T) {
+	var b bytes.Buffer
+	b.Reset()
+	b.WriteString("go语言学习")
+	t.Logf("%s\n", b.String())
+}
+
+// TestBufferTruncate 丢弃缓冲区中的所有字节，但保留前 n 个字节
+// 如果 n < 0 或 n > b.Len()，将会抛出异常
+func TestBufferTruncate(t *testing.T) {
+	defer func() {
+		if err := recover(); err != nil {
+			t.Fatal("Truncate(-1) should have panicked")
+		}
+	}()
+	var b bytes.Buffer
+	b.Truncate(-1)
+}
+
+// TestBufferUnreadByte 取消读取最近读取的字节
+func TestBufferUnreadByte(t *testing.T) {
+	var b bytes.Buffer
+	b.Write([]byte("let's go"))
+	n, err := b.ReadByte()
+	t.Logf("n = %d, err = %v\n", n, err)
+	t.Logf("b.String = %s\n", b.String())
+
+	err = b.UnreadByte()
+	if err != nil {
+		t.Fatalf("UnreadByte err: %v\n", err)
+	}
+	t.Logf("b.String = %s\n", b.String())
+
+	line, err := b.ReadString('\'')
+	t.Logf("line = %s, err = %v\n", line, err)
+	t.Logf("b.String = %s\n", b.String())
+
+	err = b.UnreadByte()
+	if err != nil {
+		t.Fatalf("UnreadByte err: %v\n", err)
+	}
+	t.Logf("b.String = %s\n", b.String())
+}
+
+// TestBufferUnreadRune 取消读取最后一个 utf-8 字符
+func TestBufferUnreadRune(t *testing.T) {
+	b := bytes.NewBuffer([]byte("一二三四五六七八九十"))
+	_, _, err := b.ReadRune()
+	if err != nil {
+		t.Fatalf("ReadRune err: %v", err)
+	}
+	err = b.UnreadRune()
+	if err != nil {
+		t.Fatalf("UnreadRune err: %v", err)
+	}
+	t.Logf("String: %s\n", b.String())
+}
+
+// TestBufferWrite 写入
+func TestBufferWrite(t *testing.T) {
+	var b bytes.Buffer
+	_, err := b.Write([]byte("Go is simple secure scalable language"))
+	if err != nil {
+		t.Fatalf("Write err: %v\n", err)
+	}
+}
+
+// TestBufferWriteByte 写入字节
+func TestBufferWriteByte(t *testing.T) {
+	var b bytes.Buffer
+	b.WriteByte('a')
+	b.WriteByte('b')
+	b.WriteByte('c')
+	b.WriteByte('d')
+	t.Logf("WriteByte: %s\n", b.String())
+}
+
+// TestBufferWriteRune 写入 rune 字符
+func TestBufferWriteRune(t *testing.T) {
+	var b bytes.Buffer
+	b.WriteRune('你')
+	b.WriteRune('好')
+	b.WriteRune('世')
+	b.WriteRune('界')
+	t.Logf("WriteRune: %s\n", b.String())
+}
+
+// TestBufferWriteString 写入字符串
+func TestBufferWriteString(t *testing.T) {
+	b := new(bytes.Buffer)
+	b.WriteString("别来无恙")
+	t.Logf("WriteString: %s\n", b.String())
+}
+
+// TestBufferWriteTo 将数据写入 w
+func TestBufferWriteTo(t *testing.T) {
+	b1 := bytes.NewBuffer([]byte("Sleep fragrance"))
+	b2 := new(bytes.Buffer)
+	_, err := b1.WriteTo(b2)
+	if err != nil {
+		t.Fatalf("WriteTo err: %v\n", err)
+	}
+}
+
+// TestNewReader 返回一个新 reader
+func TestNewReader(t *testing.T) {
+	reader := bytes.NewReader([]byte("123456"))
+	if reader.Len() != 6 {
+		t.Fatalf("Want 6, got %d\n", reader.Len())
+	}
+}
+
+// TestReaderRead 读取
+func TestReaderRead(t *testing.T) {
+	reader := bytes.NewReader([]byte("123456"))
+	buf := make([]byte, 3)
+	_, err := reader.Read(buf)
+	if err != nil {
+		t.Fatalf("Read err: %v\n", err)
+	}
+}
+
+// TestReaderReadAt 返回从指定偏移量读取的数据
+func TestReaderReadAt(t *testing.T) {
+	reader := bytes.NewReader([]byte("Go语言学习"))
+	p := make([]byte, 6)
+	n, err := reader.ReadAt(p, 2)
+	if err != nil {
+		t.Fatalf("ReadAt err: %v", err)
+	}
+	t.Logf("ReadAt: %s\n", p[:n])
+}
+
+// TestReadReadRune 读取 utf-8 编码的字符
+func TestReaderReadRune(t *testing.T) {
+	reader := bytes.NewReader([]byte("123456你好"))
+	for {
+		c, size, err := reader.ReadRune()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			t.Fatalf("ReadRune err: %v\n", err)
+		}
+		t.Logf("c = %s, size = %d\n", string(c), size)
+	}
+}
+
+// TestReaderReset 将 Reader 重置为从 b 读取
+func TestReaderReset(t *testing.T) {
+	var b bytes.Reader
+	b.Reset([]byte("123456"))
+	buf, err := io.ReadAll(&b)
+	if err != nil {
+		t.Fatalf("Got err: %v\n", err)
+	}
+	t.Logf("buf = %s\n", buf)
+}
+
+// TestReaderSeek 设置下次读取的位置
+func TestReaderSeek(t *testing.T) {
+	b := bytes.NewReader([]byte("123456"))
+	n, err := b.Seek(2, io.SeekStart)
+	t.Logf("n = %d, err = %v\n", n, err)
+
+	c, err := b.ReadByte()
+	t.Logf("byte = %s, err = %v\n", string(c), err)
+
+	b1 := bytes.NewReader([]byte("abcdefg"))
+	b1.ReadByte()
+	b1.ReadByte()
+	n, err = b1.Seek(1, 1)
+	c, _ = b1.ReadByte()
+	if string(c) != "d" {
+		t.Fatalf("Want d, got %s\n", string(c))
+	}
+
+	b2 := bytes.NewReader([]byte("abcdefg"))
+	n, _ = b2.Seek(-2, 2)
+	c, _ = b2.ReadByte()
+	if string(c) != "f" {
+		t.Fatalf("Want f, got %s\n", string(c))
+	}
 }
