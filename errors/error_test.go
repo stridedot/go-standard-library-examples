5bac7fc21e1d2529d2253f80fe5b468a8626e6ff
Onehitieoa <1195775472@qq.com>
2023-08-13 21:10:54 +0800

    flag

--- errors/error_test.go
+++ errors/error_test.go
@@ -1,1 +1,91 @@
 package errors_test
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+func TestErrorNew(t *testing.T) {
+	if errors.New("abc") == errors.New("abc") {
+		t.Log(`New("abc") == New("abc")`)
+	} else {
+		t.Log(`New("abc") != New("abc")`)
+	}
+	if errors.New("abc") != errors.New("xyz") {
+		t.Log(`New("abc") != New("xyz")`)
+	}
+
+	err := errors.New("abc")
+	if err == err {
+		t.Log(`err == err`)
+	}
+
+	if err.Error() != "abc" {
+		t.Errorf(`New("abc").Error() = %q, want %q`, err.Error(), "abc")
+	}
+}
+
+// 测试被包装过的error是否包含指定错误
+func TestErrorIs(t *testing.T) {
+	var BaseErr = errors.New("base error")
+
+	err1 := fmt.Errorf("wrap base: %w", BaseErr)
+	err2 := fmt.Errorf("wrap err1: %w", err1)
+
+	t.Logf("err1 == BaseErr: %v", err1 == BaseErr)
+
+	if errors.Is(err2, BaseErr) {
+		t.Log("err2 is BaseErr")
+		return
+	}
+
+	t.Log("err2 is not BaseErr")
+}
+
+type TypicalErr struct {
+	e string
+}
+
+func (te TypicalErr) Error() string {
+	return te.e
+}
+
+// 测试被包装过的error是否为指定类型
+// 具体说明：提取指定类型的错误，判断包装的 error 链中，
+// 某一个 error 的类型是否与 target 相同，并提取第一个符合目标类型的错误的值，
+// 将其赋值给 target
+func TestErrorAs(t *testing.T) {
+	err := TypicalErr{"typical error"}
+	err1 := fmt.Errorf("wrap err: %w", err)
+	err2 := fmt.Errorf("wrap err1: %w", err1)
+
+	var e TypicalErr
+	t.Logf("err == e: %v", err == e)
+
+	if errors.As(err2, &e) {
+		t.Log("TypicalErr is on the chain of err2")
+		return
+	}
+
+	t.Log("TypicalErr is not on the chain of err2")
+}
+
+// 测试集合多个错误
+func TestErrorJoin(t *testing.T) {
+	err1 := errors.New("err1")
+	err2 := errors.New("err2")
+	err := errors.Join(err1, err2)
+	t.Logf("err: %v", err)
+	t.Logf("err is err1: %v", errors.Is(err, err1))
+	t.Logf("err is err2: %v", errors.Is(err, err2))
+}
+
+// 暴露内部的Err，而配合使用的还有Is和As两个方法
+func TestErrorUnwrap(t *testing.T) {
+	err1 := errors.New("error1")
+	err2 := fmt.Errorf("error2: [%w]", err1)
+	err3 := fmt.Errorf("error3: [%w]", err2)
+	t.Logf("err3: %v", err3)
+	t.Logf("errors.Unwrap(err3): %v", errors.Unwrap(err3))
+}
--- flag/flag_test.go
+++ flag/flag_test.go
@@ -2,6 +2,9 @@ package flag_test
 
 import (
 	"flag"
+	"fmt"
+	"strconv"
+	"strings"
 	"testing"
 	"time"
 )
@@ -32,10 +35,79 @@ func TestMain(m *testing.M) {
 	var durationVarArg time.Duration
 	flag.DurationVar(&durationVarArg, "arg4", 0, "This is a duration argument")
 
+	// 带默认值和提示语句的float64类型flag，例如 -arg5=5.5
+	float64ArgPtr := flag.Float64("arg5", 4.4, "This is a float64 argument")
+
+	var float64VarArg float64
+	flag.Float64Var(&float64VarArg, "arg6", 4.4, "This is a float64 argument")
+
+	// 带默认值和提示语句的int类型flag，例如 -arg7=5
+	intArgPtr := flag.Int("arg7", 0, "This is a int argument")
+
+	var intVarArg int
+	flag.IntVar(&intVarArg, "arg8", 0, "This is a int argument")
+
+	// 带默认值和提示语句的int64类型flag，例如 -arg9=5
+	int64ArgPtr := flag.Int64("arg9", 0, "This is a int64 argument")
+
+	var int64VarArg int64
+	flag.Int64Var(&int64VarArg, "arg10", 0, "This is a int64 argument")
+
 	flag.Parse()
 
 	println("The value of 'arg1': ", *boolArgPtr)
 	println("The value of 'arg2': ", boolVarArg)
 	println("The value of 'arg3': ", *durationArgPtr)
 	println("The value of 'arg4': ", durationVarArg)
+	fmt.Printf("The value of 'arg5':  %.2f\n", *float64ArgPtr)
+	println("The value of 'arg5': ", strconv.FormatFloat(*float64ArgPtr, 'f', 2, 64))
+	println("The value of 'arg6': ", float64VarArg)
+	println("The value of 'arg7': ", *intArgPtr)
+	println("The value of 'arg8': ", intVarArg)
+	println("The value of 'arg9': ", *int64ArgPtr)
+	println("The value of 'arg10': ", int64VarArg)
+}
+
+// 测试使用自定义命令
+// 注意：flag.Func 只能用在非测试函数中
+func TestFlagFunc(t *testing.T) {
+	// First we need to declare variables to hold the values from the
+	// command-line flags. Notice that we also need to set any defaults,
+	// which will be used if the relevant flag is not provided at runtime.
+	var (
+		urls  []string                    // Default of the empty slice
+		pause time.Duration = time.Second // Default of one second
+	)
+
+	// The flag.Func() function takes three parameters: the flag name,
+	// descriptive help text, and a function with the signature
+	// `func(string) error` which is called to process the string value
+	// from the command-line flag at runtime and assign it to the necessary
+	// variable. In this case, we use strings.Fields() to split the string
+	// based on whitespace and store the resulting slice in the urls
+	// variable that we declared above. We then return nil from the
+	// function to indicate that the flag was parsed without any errors.
+	flag.Func("urls", "List of URLs to print", func(flagValue string) error {
+		urls = strings.Fields(flagValue)
+		return nil
+	})
+
+	// Likewise we can do the same thing to parse the pause duration. The
+	// time.ParseDuration() function may throw an error here, so we make
+	// sure to return that from our function.
+	flag.Func("pause", "Duration to pause between printing URLs", func(flagValue string) error {
+		var err error
+		pause, err = time.ParseDuration(flagValue)
+		return err
+	})
+
+	// Importantly, call flag.Parse() to trigger actual parsing of the
+	// flags.
+	flag.Parse()
+
+	// Print out the URLs, pausing between each iteration.
+	for _, u := range urls {
+		t.Log(u)
+		time.Sleep(pause)
+	}
 }
